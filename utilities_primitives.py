319ee98ca4f7f540dfcc776e1da9ec588f6958e4
Chris <chris.goringe@gmail.com>
2023-09-05 14:30:34 +1000

    fixes

--- utilities_primitives.py
+++ utilities_primitives.py
@@ -31,32 +31,3 @@ class PrimitiveInt(PrimitiveString):
 class PrimitiveFloat(PrimitiveInt):
     TYPE = "FLOAT"
     DEFAULT = 0.0
-
-class PrimitiveStringPair(Base_utilities):
-    CATEGORY = "utilities/primitives"
-    TYPE = "STRING"
-    DEFAULT = ""
-
-    @classproperty
-    def REQUIRED(cls):
-        return { "value1": (cls.TYPE, {"default": cls.DEFAULT}), "value2": (cls.TYPE, {"default": cls.DEFAULT})  }
-
-    @classproperty
-    def RETURN_TYPES(cls):
-        return (cls.TYPE,cls.TYPE,)
-    
-    @classproperty
-    def RETURN_NAMES(cls):
-        return (cls.TYPE.lower()+"1",cls.TYPE.lower()+"2",)
-    
-    @classmethod
-    def func(cls, value1, value2,):
-        return (value1,value2,)
-    
-class PrimitiveIntPair(PrimitiveStringPair):
-    TYPE = "INT"
-    DEFAULT = 0
-
-class PrimitiveFloatPair(PrimitiveStringPair):
-    TYPE = "FLOAT"
-    DEFAULT = 0.0
