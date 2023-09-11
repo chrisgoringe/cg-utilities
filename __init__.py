fc79eb2832ff2aba1fd82fdefd562888389c5038
Chris <chris.goringe@gmail.com>
2023-09-12 09:24:39 +1000

    fix

--- __init__.py
+++ __init__.py
@@ -18,7 +18,9 @@ from .utilities_images import *
 from .utilities_conditioning import *
 from .utilities_strings import *
 from .utilities_passthroughs import *
-from .common import *
+from custom_nodes.cg_custom_core.base import application_web_extensions_directory
+module_root_directory = os.path.dirname(os.path.realpath(__file__))
+module_js_directory = os.path.join(module_root_directory, "js")
 
 NODE_CLASS_MAPPINGS = { 
                       # conditioning
--- common.py
+++
@@ -1,4 +0,0 @@
-import os
-
-module_root_directory = os.path.dirname(os.path.realpath(__file__))
-module_js_directory = os.path.join(module_root_directory, "js")
